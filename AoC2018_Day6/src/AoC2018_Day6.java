2cc6faacc4242bb57c7ac294a7217ada21e7cbda
Lance <lance_gettins@hotmail.com>
2018-12-08 16:15:08 -0600

    2018 try 1

---
+++ AoC2018_Day6/src/AoC2018_Day6.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class AoC2018_Day6 {
+    public static void main(String[] args) throws Exception {
+
+        String thisLine = null;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/home/lance/IdeaProjects/AoC2018_Day6_Input.txt"));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
